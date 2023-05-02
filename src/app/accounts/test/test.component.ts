65ca9ef5c9b781c0e4834f4338e61f2d7d5ab72c
khouloud rebai <khouloudrebai77@gmail.com>
2023-05-02 13:40:05 +0200

    xxx

--- src/app/accounts/test/test.component.ts
+++ src/app/accounts/test/test.component.ts
@@ -1,34 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { UserService } from '../services/user.service';
+import { LoginService } from '../services/login.service';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent   {
-  name = "Angular Toggle Show Hide";
-  showMyContainer: boolean = false;
-
-  status: boolean = false;
-  statusLink: boolean = false;
-  clickEvent() {
-    this.status = !this.status;
-    //this.statusLink = !this.statusLink;
-
-    if (this.statusLink) {
-      setTimeout(() => {
-        this.statusLink = false;
-      }, 230);
-    } else {
-      this.statusLink = true;
-    }
-  }
-}
-
-
-  
-
+export class TestComponent  implements OnInit {
  
+  email!: string;
+  pwd!:string;
+ form!: FormGroup;
+ submitted = false;
 
+ constructor(private formBuilder: FormBuilder,private router: Router,private loginService:LoginService) { 
+    this.form = this.formBuilder.group(
+  {
+   
+    email: ['', [Validators.required, Validators.email]],
+    pwd: [
+      '',
+      [
+        Validators.required,]
+    ],
+  }
+);}
+
+ ngOnInit(): void {
+ }
+ get f(): { [key: string]: AbstractControl } {
+   return this.form.controls;
+ }
+
+ onSubmit(): void {
+   this.submitted = true;
+
+   if (this.form.invalid) {
+     return;
+   }
+   console.log(this.form.value)
+   this.loginService.Contact_auth(this.form.value.email,this.form.value.pwd).subscribe(respond=>{
+    console.log(respond)
+    console.log(respond.isFailed);
+    console.log(respond.code);
+    
+    if(respond.isFailed == false && respond.code === '201' && respond.data)
+    {
+      this.router.navigate(['/acceuil']);
+
+      sessionStorage.setItem('user',JSON.stringify(respond.data));
+    }
+})   
+   
+   console.log(JSON.stringify(this.form.value, null, 2));
+ }
+
+ onReset(): void {
+   this.submitted = false;
+   this.form.reset();
+ }
+
+ forget():void {
+  this.router.navigate(['/motdepasse']);
+ }
+ 
+}
